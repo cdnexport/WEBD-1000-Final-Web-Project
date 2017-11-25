33f91bf392df1d56ba6d18714d8fb9849e63d6d5
cdnexport <fudgemaster3000@hotmail.com>
2017-11-24 20:34:58 -0600

    'Eraser' now just paints white

--- js/WebPaint.js
+++ js/WebPaint.js
@@ -198,12 +198,12 @@ function mouseMoveHandler(e,mousedown,color,willErase){
 		let width = document.getElementById("selectSize").value;
 		ctx.lineWidth = width;
 		if(willErase){
-			ctx.clearRect(mouseX,mouseY,width,width);
+			ctx.strokeStyle = "rgb(255,255,255)";
 		}
 		else{
 			ctx.strokeStyle = color;
-			ctx.stroke();
 		}
+		ctx.stroke();
 	}
 }
 //Set the background color of the colorOptions list to the active slider color.
